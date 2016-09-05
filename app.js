187fcf2650a5af698f0af2b45a258a49ac4fb67d
pfefferf <florian.pfeffer@outlook.com>
2016-09-05 12:38:20 +0200

    Update

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
+let cfenv = require('cfenv');
 
 // create express instance
 let oApp = express();
+
+// Cloud Foundry environment variables
+let oAppEnv = cfenv.getAppEnv();
 
 // body parser middleware to handle URL parameter and JSON bodies
 oApp.use(bodyParser.urlencoded({extended: false}));
 oApp.use(bodyParser.json());
 
 // express app listener
-let port = process.env.PORT || 3000;
-
-oApp.listen(port, function(){
-    console.log('Server listening at ' + port);
+oApp.listen(oAppEnv.port, function(){
+    console.log('Server listening at ' + oAppEnv.url);
 });
